ff24c09670efbb0364f090039faa8c528867b154
María RN <mraneranufv@gmail.com>
2024-09-21 15:20:31 +0100

    Change between ingredients in pantry [done + optim]

--- Assets/Scripts/PantryBehaviour.cs
+++ Assets/Scripts/PantryBehaviour.cs
@@ -5,7 +5,38 @@ using UnityEngine;
 public class PantryBehaviour : MonoBehaviour
 {
     // ---------------------------------- ARRAYS -------------------------------------------
+    // Array to store all the Ingredient chips GOs
     public GameObject[] pIngredientChip;
+
+    // Array to store all the ingredient chips with the default sprite
+    public Sprite[] pIngredientDefault;
+
+    // Ingredient chips default sprite
+    public Sprite PTomato;
+    public Sprite PCarrot;
+    public Sprite PEggplant;
+    public Sprite PMushroom;
+
+    // ---------------------------------- SCRIPTS -----------------------------------------
+    // Access Clicker script
+    public Clicker ClickerScript;
+
+
+    // ---------------------------------- AT THE START OF THE GAME ------------------------------------------
+    void Start()
+    {
+        // ---------------------------------- SET ARRAYS ----------------------------------------
+        // Add all the sprites to the Pantry's Ingredients (default) array
+        pIngredientDefault[0] = PTomato;
+        pIngredientDefault[1] = PCarrot;
+        pIngredientDefault[2] = PEggplant;
+        pIngredientDefault[3] = PMushroom;
+
+        // ---------------------------------- ACCESS --------------------------------------------
+        // Access the Clicker script
+        ClickerScript = FindObjectOfType<Clicker>();
+
+    }
 
 
     // ---------------------------------- DEACTIVATE INGREDIENTS ------------------------------------------
@@ -19,6 +50,12 @@ public class PantryBehaviour : MonoBehaviour
             {
                 // Mark it as an ingredient NOT SELECTED
                 pIngredientChip[i].transform.parent.GetComponent<Clicker>().isIngrSelected = false;
+
+                // Set the sprites of the none selected ingredients as default ones
+                pIngredientChip[i].GetComponent<SpriteRenderer>().sprite = pIngredientDefault[i];
+
+                // Deactivate the none selected ingredients' shapes
+                ClickerScript.cIngredientShape[i].SetActive(false);
             }
         }
     }
