bb51dca2586a4749a6cf6c2d0cb5be72026b1231
María RN <mraneranufv@gmail.com>
2024-09-30 13:49:00 +0100

    [DONE d vdd] Backpack grid

---
+++ Assets/Scripts/UIButtons.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class UIButtons : MonoBehaviour
+{
+    /*public void StartGame()
+    {
+        SceneManager.LoadScene("Calendar");
+    }*/
+
+
+    // When Calendar Button (in-game) gets clicked
+    public void CalendarButton()
+    {
+        // Change the Scene to the Calendar one
+        //SceneManager.LoadScene("Calendar");
+    }
+
+
+    // When Backpack Button (in-game) gets clicked
+    public void BackpackButton()
+    {
+        // Change the Scene to the Backpack one
+        //SceneManager.LoadScene("Backpack");
+    }
+
+
+    // When Map Button (in-game) gets clicked
+    public void MapButton()
+    {
+        // Change the Scene to the Map one
+        //SceneManager.LoadScene("Map");
+    }
+
+
+    /*public void ReturnGame()
+    {
+        SceneManager.LoadScene("Menú");
+    }*/
+
+    /*public void CreditsGame()
+    {
+        SceneManager.LoadScene("Credits");
+    }*/
+
+    /*public void ExitGame()
+    {
+        Debug.Log("EXIT");
+        Application.Quit();
+    }*/
+}
